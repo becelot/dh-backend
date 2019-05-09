49980c327dd240f4a0d761ad4aa7db0097c2042f
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-09 14:09:35 +0200

    Added hearthstone data dependencies

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
+from hearthstone import cardxml
+from hearthstone.deckstrings import Deck
+
 from dh_backend import create_app
+
+deck = Deck.from_deckstring("AAECAf0ECsUE7QWQB+wHvwj7DKCAA6aHA8CYA4qeAwpNigG7AskDqwTLBJYF4Qe+7AKDlgMA")
+deckId = deck.get_dbf_id_list()
+dbf_db, _ = cardxml.load_dbf()
+
+print(dbf_db[deckId[0][0]].card_id)
 
 if __name__ == '__main__':
     app = create_app()
