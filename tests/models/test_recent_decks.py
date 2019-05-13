b855d687719c63cd572f7d9f0a131873151440f5
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-13 21:55:36 +0200

    Test cases for recent deck update function

--- tests/models/test_recent_decks.py
+++ tests/models/test_recent_decks.py
@@ -1,4 +1,4 @@
-from dh_backend.models import RecentDeck
+from dh_backend.models import RecentDeck, Deck
 from tests.data.decks import new_deck
 from tests.data.users import test_user
 
@@ -61,3 +61,50 @@ def test_recent_user_one_to_one(db_session):
     db_session.commit()
 
     assert len(db_session.query(RecentDeck).filter_by(user_id=user.id).all()) == 1
+
+
+def test_recent_decks_update(db_session):
+    user = test_user()
+    db_session.add(user)
+    db_session.commit()
+
+    recent = RecentDeck(user=user)
+    db_session.add(recent)
+    db_session.commit()
+
+    decks = [
+        Deck(user=user),
+        Deck(user=user),
+        Deck(user=user),
+        Deck(user=user),
+        Deck(user=user),
+        Deck(user=user),
+        Deck(user=user)
+    ]
+    for deck in decks:
+        db_session.add(deck)
+    db_session.commit()
+
+    validity_set = [
+        (0, (0, None, None, None, None)),
+        (1, (1, 0, None, None, None)),
+        (2, (2, 1, 0, None, None)),
+        (3, (3, 2, 1, 0, None)),
+        (4, (4, 3, 2, 1, 0)),
+        (5, (5, 4, 3, 2, 1)),
+        (2, (2, 5, 4, 3, 1)),
+        (1, (1, 2, 5, 4, 3)),
+        (6, (6, 1, 2, 5, 4)),
+        (6, (6, 1, 2, 5, 4)),
+        (1, (1, 6, 2, 5, 4)),
+        (2, (2, 1, 6, 5, 4)),
+    ]
+
+    for test in validity_set:
+        recent.set_recent_deck(decks[test[0]])
+
+        assert recent.current_deck == (None if test[1][0] is None else decks[test[1][0]])
+        assert recent.previous_deck == (None if test[1][1] is None else decks[test[1][1]])
+        assert recent.deck_3 == (None if test[1][2] is None else decks[test[1][2]])
+        assert recent.deck_4 == (None if test[1][3] is None else decks[test[1][3]])
+        assert recent.deck_5 == (None if test[1][4] is None else decks[test[1][4]])
