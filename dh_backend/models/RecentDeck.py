e4083ee5252e888f5ac6c56d10aec00b3d64bb79
BBecker <benedict.becker@rwth-aachen.de>
2019-05-13 16:23:01 +0200

    Added set_recent_deck to dynamically update the list of decks that a user played

--- dh_backend/models/RecentDeck.py
+++ dh_backend/models/RecentDeck.py
@@ -24,3 +24,32 @@ class RecentDeck(db.Model):
 
     deck_5_id = db.Column(db.Integer, db.ForeignKey("Deck.id"), nullable=True)
     deck_5 = db.relationship("Deck", foreign_keys=[deck_5_id])
+
+    def set_recent_deck(self, deck: Deck):
+        """
+        Update the list of decks with a new deck
+        :param deck: The deck to insert into the list
+        """
+        if deck == self.current_deck:
+            return
+
+        tmp = self.previous_deck
+        self.previous_deck = self.current_deck
+        self.current_deck = deck
+
+        if deck == tmp:
+            return
+
+        tmp2 = self.deck_3
+        self.deck_3 = tmp
+
+        if deck == tmp2:
+            return
+
+        tmp = self.deck_4
+        self.deck_4 = tmp2
+
+        if deck == tmp:
+            return
+
+        self.deck_5 = tmp
