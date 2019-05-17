0a58079d6bbe3c70716893a46f1133a7fd0b5477
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-17 20:59:07 +0200

    Added new twitch session resource

--- dh_backend/api/auth/__init__.py
+++ dh_backend/api/auth/__init__.py
@@ -1,7 +1,9 @@
 from flask_restful import Api
 
+from dh_backend.api.auth import TwitchSession
 from dh_backend.api.auth.LoginUser import LoginUser
 
 
 def load_auth_api(api: Api, prefix):
     api.add_resource(LoginUser, f"{prefix}/login")
+    api.add_resource(TwitchSession, f"{prefix}/twitch_session")
