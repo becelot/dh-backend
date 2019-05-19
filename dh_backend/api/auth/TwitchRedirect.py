fc112bd903c8372c12c90888728b9c67ff526aeb
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-19 19:18:27 +0200

    Fixed bug where authorization failures were not detected

--- dh_backend/api/auth/TwitchRedirect.py
+++ dh_backend/api/auth/TwitchRedirect.py
@@ -13,10 +13,16 @@ class TwitchRedirect(Resource):
     parser = RequestParser()\
         .add_argument('code', type=str, location='args')\
         .add_argument('scope', type=str, location='args')\
-        .add_argument('state', type=str, location='args')
+        .add_argument('state', type=str, location='args')\
+        .add_argument('error', type=str, location="args")\
+        .add_argument('error_description', type=str, location="args")
 
     def get(self):
         args = TwitchRedirect.parser.parse_args()
+
+        authorized, _ = twitch.auth_flow().validate_redirect_authorization()
+        if not authorized:
+            return "Authorization failed. Invalid session."
 
         session = args['state']
         if not session:
@@ -24,9 +30,6 @@ class TwitchRedirect(Resource):
 
         twitch_session: TwitchSession = TwitchSession.query.filter_by(twitch_session_token=session).first()
         if not twitch_session:
-            return "Authorization failed. Invalid session."
-
-        if not twitch.auth_flow().validate_redirect_authorization():
             return "Authorization failed. Invalid session."
 
         account: TwitchAccount = TwitchAccount.from_user(twitch_session.user)
